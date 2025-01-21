6832d7919dcec18bfc0fe3b8276e0de2feee0825
sistracia <156994197+sistracia@users.noreply.github.com>
2025-01-21 19:19:17 +0700

    Add immediatelyRender prop to TailwindAdvancedEditor component

--- packages/editor/dist/advance-editor.d.ts
+++ packages/editor/dist/advance-editor.d.ts
@@ -1,7 +1,8 @@
 import { type JSONContent } from '@hooore/editor-headless/components';
 export type TailwindAdvancedEditorProps = {
     initialValue?: JSONContent;
+    immediatelyRender?: boolean;
     onChange: (value: JSONContent) => void;
 };
-export declare const TailwindAdvancedEditor: ({ initialValue, onChange, }: TailwindAdvancedEditorProps) => import("react/jsx-runtime").JSX.Element;
+export declare const TailwindAdvancedEditor: ({ initialValue, immediatelyRender, onChange, }: TailwindAdvancedEditorProps) => import("react/jsx-runtime").JSX.Element;
 export default TailwindAdvancedEditor;
